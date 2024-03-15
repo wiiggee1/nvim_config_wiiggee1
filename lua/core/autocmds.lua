5076bfae746c53b87a58a157ad3259cd845a7a61
Viktor Smedberg <95021602+wiiggee1@users.noreply.github.com>
2024-03-15 16:49:52 +0100

    Delete lua directory

--- lua/core/autocmds.lua
+++
@@ -1,8 +0,0 @@
--- Auto reloading neovim session listening for event whenever buffer is being written to
-local autocmd = vim.api.nvim_create_autocmd
-
-
-
-autocmd('BufWritePost', {
-    command = 'so'
-})
